c5279a9432eaf4693c77db024288c2595994b3f3
Keith <keith.githinji@student.moringaschool.com>
2025-01-27 18:38:26 +0300

    Created RecipeResultsPage component with basic layout and placeholder content.

---
+++ client/src/pages/RecipeResultsPage.js
@@ -0,0 +1,13 @@
+// src/pages/RecipeResultsPage.js
+import React from 'react';
+
+function RecipeResultsPage() {
+  return (
+    <div>
+      <h1>Recipe Results</h1>
+      <p>Here are the recipes based on your search criteria.</p>
+    </div>
+  );
+}
+
+export default RecipeResultsPage;
