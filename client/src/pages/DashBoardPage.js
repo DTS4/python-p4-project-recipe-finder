5520ddb3fee46e46b5f4414c18f49ef74000ddab
Keith <keith.githinji@student.moringaschool.com>
2025-01-27 18:33:16 +0300

    Created DashboardPage component with basic layout and placeholder content.

---
+++ client/src/pages/DashBoardPage.js
@@ -0,0 +1,13 @@
+// src/pages/DashboardPage.js
+import React from 'react';
+
+function DashboardPage() {
+  return (
+    <div>
+      <h1>Your Dashboard</h1>
+      <p>Here you can manage your favorite recipes and other preferences.</p>
+    </div>
+  );
+}
+
+export default DashboardPage;
